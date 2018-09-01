03d522729c6ec31105ebc44c32f5822b145900b0
Michael Palhidai <mpalhidai@gmail.com>
2018-08-31 19:52:37 -0700

    fixed add book render

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,7 @@
 let myFirstBook = new Book("One Fish Two Fish Red Fish Blue Fish","Dr. Seuss",66,true);
 let mySecondBook = new Book("Green Eggs and Ham","Dr. Seuss",72,true);
 let myLibrary = [myFirstBook, mySecondBook];
+let index = 0;
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -27,24 +28,31 @@ function addBookToLibrary() {
 }
 
 function render(books) {
+
+  // have an outer container and if rendered delete it before continuing
+  //  or have add book add a row to table?
+  // if (booksContainer) { mainContainer.removeChild(booksContainer); }
+
   let table = document.getElementById("Library");
-  for (let i = 0; i < books.length; i++) {
+  while (index < books.length) {
     let row = table.insertRow();
 
     let indexColumn = document.createElement("TH");
     indexColumn.setAttribute("scope", "row");
-    indexColumn.innerHTML = i+1;
+    indexColumn.innerHTML = index+1;
     row.appendChild(indexColumn);
 
 
     let titleColumn = row.insertCell(1);
-    titleColumn.innerHTML = books[i].title;
+    titleColumn.innerHTML = books[index].title;
     let authorColumn = row.insertCell(2);
-    authorColumn.innerHTML = books[i].author;
+    authorColumn.innerHTML = books[index].author;
     let pagesColumn = row.insertCell(3);
-    pagesColumn.innerHTML = books[i].pages;
+    pagesColumn.innerHTML = books[index].pages;
     let readColumn = row.insertCell(4);
-    readColumn.innerHTML = books[i].read;
+    readColumn.innerHTML = books[index].read;
+
+    index++;
   }
 }
 
